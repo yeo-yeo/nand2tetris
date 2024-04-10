26df74a1d647a5a0b495c6748b9c7a33b86105de
Gillian <gillian@yeomans.org.uk>
2024-04-09 22:51:35 -0400

    week 4

--- 4/fill/Fill.asm
+++ 4/fill/Fill.asm
@@ -8,4 +8,42 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+// Initial setup: declare and set some pointers to 0
+@0
+D=A // set D as constant 0
+@lastvalue
+M=D // set lastvalue variable as 0
+
+(MAINLOOP)
+    @KBD
+    D=M
+    @lastvalue 
+    D=D-M // check last keyboard value vs current
+    @MAINLOOP 
+    D;JEQ // if it's 0 nothing's changed - keep doing this loop 
+
+// could give this section a name too
+// we're here if something did change and we need to repaint
+@KBD
+D=M
+@lastvalue 
+M=D // update lastvalue to be this one 
+(PAINT)
+    @8192 // total iterations (32 words per row, 256 rows) (should be 8192)
+    D=A
+    @wordcountpointer
+    M=D
+    (PAINTMAGIC)
+        @wordcountpointer
+        M=M-1
+        @SCREEN
+        D=A
+        @wordcountpointer
+        A=D+M // make the current address D (screen base address) plus current wordcountpointer
+        M=!M // make it other!
+        @wordcountpointer
+        D=M
+        @PAINTMAGIC
+        D;JGT // if wordcount pointer is above 0, keep looping!
+@MAINLOOP
+0;JMP
--- 4/mult/Mult.asm
+++ 4/mult/Mult.asm
@@ -7,4 +7,52 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// General plan: write a loop that runs RAM[0] times, adding RAM[1] each time
+// to a working sum, which then gets output at RAM[2]
+// Potential optimisation: do a comparison to see which is smaller out of RAM[0]
+// and RAM[1] and choose that to be i and the other to be the amount to add each 
+// time?
+
+(SETUP) // Initialising i and working sum 
+    @0 // load constant value 0 into A
+    D=A // set D to A (0)
+    @workingsum // initialise this variable with 0
+    M=D
+    @R0 // in case i is 0 to start with, skip loop
+    D=M
+    @SETOUTPUT
+    D;JEQ 
+
+(LOOP)
+    @R1 
+    D=M // put R1 into D
+    @workingsum
+    M=D+M // add D (R1) to the current working sum
+    @R0
+    M=M-1 // dec R[0] which is like our i and save back
+    D=M // set D to new i
+    @LOOP
+    D;JGT // if i is >0, we need to loop again
+    // NB you can also do like D-1;JGT which is jump if D-1 is greather than 0 - could have used that instead. would then need to save down D at the beginning of the loop so would need to think about that when setting up initial loop too.
+
+// if we get here it should be because we finished looping
+// which means we need to store sum into R2
+(SETOUTPUT)
+    @workingsum
+    D=M
+    @R2
+    M=D
+    @END
+    0;JMP
+
+// Funky infinite loop bit to stop someone sneakily exploiting our code
+(END)
+    @END
+    0;JMP
+
+// Things I encountered: new line gaps in labels end them - I did this by accident at first
+// when I was just trying to space out my notes
+// To make my code pass the clock time constraints of the tests I had to rewrite it to
+// modify one of the inputs (instead of declaring a separate variable for i) - this feels
+// kind of wrong to me on principle but I guess you want to be as efficient as 
+// possible when writing machine code
